12f533f7d3b586368bc5ae0721604c0cd2184319
Hagen Paul Pfeifer <hagen@jauu.net>
2013-02-25 17:08:56 +0100

    indent code cleanup
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- futex-locking/futex-locking.c
+++ futex-locking/futex-locking.c
@@ -24,8 +24,6 @@ static int mutex_type = MUTEX_TYPE_FUTEX_VANILLA;
 static pthread_mutex_t pth_mutex = PTHREAD_MUTEX_INITIALIZER;
 static int x_mutex;
 
-
-
 #define atomic_xadd(P, V) __sync_fetch_and_add((P), (V))
 #define cmpxchg(P, O, N) __sync_val_compare_and_swap((P), (O), (N))
 #define atomic_inc(P) __sync_add_and_fetch((P), 1)
@@ -33,7 +31,6 @@ static int x_mutex;
 #define atomic_add(P, V) __sync_add_and_fetch((P), (V))
 #define atomic_set_bit(P, V) __sync_or_and_fetch((P), 1<<(V))
 #define atomic_clear_bit(P, V) __sync_and_and_fetch((P), ~(1<<(V)))
-
 
 /* Compile read-write barrier */
 #define barrier() asm volatile("": : :"memory")
@@ -44,30 +41,28 @@ static int x_mutex;
 /* Atomic exchange (of various sizes) */
 static inline void *xchg_64(void *ptr, void *x)
 {
-	__asm__ __volatile__("xchgq %0,%1"
-				:"=r" ((unsigned long long) x)
-				:"m" (*(volatile long long *)ptr), "0" ((unsigned long long) x)
-				:"memory");
+	__asm__ __volatile__("xchgq %0,%1":"=r"((unsigned long long)x)
+			     :"m"(*(volatile long long *)ptr),
+			     "0"((unsigned long long)x)
+			     :"memory");
 
 	return x;
 }
 
 static inline unsigned xchg_32(void *ptr, unsigned x)
 {
-	__asm__ __volatile__("xchgl %0,%1"
-				:"=r" ((unsigned) x)
-				:"m" (*(volatile unsigned *)ptr), "0" (x)
-				:"memory");
+	__asm__ __volatile__("xchgl %0,%1":"=r"((unsigned)x)
+			     :"m"(*(volatile unsigned *)ptr), "0"(x)
+			     :"memory");
 
 	return x;
 }
 
 static inline unsigned short xchg_16(void *ptr, unsigned short x)
 {
-	__asm__ __volatile__("xchgw %0,%1"
-				:"=r" ((unsigned short) x)
-				:"m" (*(volatile unsigned short *)ptr), "0" (x)
-				:"memory");
+	__asm__ __volatile__("xchgw %0,%1":"=r"((unsigned short)x)
+			     :"m"(*(volatile unsigned short *)ptr), "0"(x)
+			     :"memory");
 
 	return x;
 }
@@ -77,23 +72,21 @@ static inline char atomic_bitsetandtest(void *ptr, int x)
 {
 	char out;
 	__asm__ __volatile__("lock; bts %2,%1\n"
-						"sbb %0,%0\n"
-				:"=r" (out), "=m" (*(volatile long long *)ptr)
-				:"Ir" (x)
-				:"memory");
+			     "sbb %0,%0\n":"=r"(out),
+			     "=m"(*(volatile long long *)ptr)
+			     :"Ir"(x)
+			     :"memory");
 
 	return out;
 }
 
-
 static volatile int shared_tmp;
 
-int sys_futex(void *addr1, int op, int val1, struct timespec *timeout, void *addr2, int val3)
+int sys_futex(void *addr1, int op, int val1, struct timespec *timeout,
+	      void *addr2, int val3)
 {
 	return syscall(SYS_futex, addr1, op, val1, timeout, addr2, val3);
 }
-
-
 
 #define MUTEX_INITIALIZER {0}
 
@@ -107,14 +100,12 @@ void x_mutex_destroy(void)
 	return;
 }
 
-
 void x_mutex_lock_ext(void)
 {
 	int i, c;
 
 	/* Spin and try to take lock */
-	for (i = 0; i < 100; i++)
-	{
+	for (i = 0; i < 100; i++) {
 		c = cmpxchg(&x_mutex, 0, 1);
 		if (!c)
 			return;
@@ -123,7 +114,8 @@ void x_mutex_lock_ext(void)
 	}
 
 	/* The lock is now contended */
-	if (c == 1) c = xchg_32(&x_mutex, 2);
+	if (c == 1)
+		c = xchg_32(&x_mutex, 2);
 
 	while (c) {
 		/* Wait in the kernel */
@@ -134,24 +126,19 @@ void x_mutex_lock_ext(void)
 	return;
 }
 
-
 void x_mutex_unlock_ext(void)
 {
 	int i;
 
 	/* Unlock, and if not contended then exit. */
-	if (x_mutex == 2)
-	{
+	if (x_mutex == 2) {
 		x_mutex = 0;
-	}
-	else if (xchg_32(&x_mutex, 0) == 1)
+	} else if (xchg_32(&x_mutex, 0) == 1)
 		return;
 
 	/* Spin and hope someone takes the lock */
-	for (i = 0; i < 200; i++)
-	{
-		if (x_mutex)
-		{
+	for (i = 0; i < 200; i++) {
+		if (x_mutex) {
 			/* Need to set to state 2 because there may be waiters */
 			if (cmpxchg(&x_mutex, 1, 2))
 				return;
@@ -164,7 +151,6 @@ void x_mutex_unlock_ext(void)
 
 	return;
 }
-
 
 void x_mutex_lock(void)
 {
@@ -179,7 +165,6 @@ void x_mutex_lock(void)
 	return;
 }
 
-
 void x_mutex_unlock(void)
 {
 	int i, ret;
@@ -191,10 +176,8 @@ void x_mutex_unlock(void)
 		return;
 
 	/* Spin and hope someone takes the lock */
-	for (i = 0; i < 1; i++)
-	{
-		if (x_mutex)
-		{
+	for (i = 0; i < 1; i++) {
+		if (x_mutex) {
 			/* Need to set to state 2 because there may be waiters */
 			if (cmpxchg(&x_mutex, 1, 2))
 				return;
@@ -205,10 +188,8 @@ void x_mutex_unlock(void)
 	/* We need to wake someone up */
 	ret = sys_futex(&x_mutex, FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
 
-
 	return;
 }
-
 
 /* ~~1 second */
 static void busy_loop(double mult)
@@ -228,7 +209,7 @@ static void msleep(unsigned long milisec)
 
 	time_t sec = milisec / 1000;
 
-	milisec = milisec - ( sec * 1000);
+	milisec = milisec - (sec * 1000);
 	req.tv_sec = sec;
 	req.tv_nsec = milisec * 1000000L;
 
@@ -283,11 +264,10 @@ static void unlock(void)
 	}
 }
 
-
 static void *locker(void *args)
 {
 	int i;
-	int num = (int) *((int *) args);
+	int num = (int)*((int *)args);
 
 	fprintf(stderr, "# %d alive\n", num);
 
@@ -307,14 +287,12 @@ static void *locker(void *args)
 	return NULL;
 }
 
-
 static void *unlocker(void *args)
 {
 	int i;
-	int num = (int) *((int *) args);
+	int num = (int)*((int *)args);
 
 	fprintf(stderr, "# %d alive\n", num);
-
 
 	for (i = 0; i < LOOK_ROUNDS; i++) {
 		lock();
@@ -330,14 +308,10 @@ static void *unlocker(void *args)
 	return NULL;
 }
 
-
-
 int main(int ac, char **av)
 {
 	int ret = 0, *num;
 	pthread_t thread_id[2];
-
-
 
 	num = malloc(sizeof(int));
 	if (!num) {
@@ -368,11 +342,7 @@ int main(int ac, char **av)
 	pthread_join(thread_id[0], NULL);
 	pthread_join(thread_id[1], NULL);
 
-
 	fprintf(stderr, "# exiting\n");
 
 	return EXIT_SUCCESS;
 }
-
-
-
